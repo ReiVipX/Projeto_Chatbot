47855806980fbbd2067eeb08bd72a119c801f26c
ReiVipX <lucasgrcayres@gmail.com>
2023-06-03 21:57:53 -0300

    Att do script + textos do CHATBOT

--- chatbot.js
+++ chatbot.js
@@ -11,7 +11,8 @@ const chatbot_window = document.querySelector("div[class='chatbot_window']");
 const chatbot_button = document.getElementById('botao')
 const chatbot_input = document.getElementById('chatbot_input_text');
 const chatbot_text = document.getElementById('chatbot_text_id');
-const chatbot_memory = "https://raw.githubusercontent.com/ReiVipX/Projeto_Chatbot/main/chatbot_memory.json?token=GHSAT0AAAAAACCA7RAC634QMQTUB5JURRKCZDJJYPQ";
+//const chatbot_memory = "https://raw.githubusercontent.com/ReiVipX/Projeto_Chatbot/main/chatbot_memory.json?token=GHSAT0AAAAAACCA7RAC634QMQTUB5JURRKCZDJJYPQ";
+const chatbot_memory = "/Mark1-Projeto/chatbot_memory.json"
 
 //
 //
@@ -21,20 +22,22 @@ const chatbot_memory = "https://raw.githubusercontent.com/ReiVipX/Projeto_Chatbo
 //
 //
 
+// Função para exibir a janela do chatbot
 function open_chatbot(){
   chatbot_button.style.cssText = 'display: none'
   chatbot_window.style.cssText = 'display: flex'
-  let text = "inicio"
-
-  searchJsonFile(text)
+  searchJsonFile("inicio")
 }
-function exit_chatbot() {   
+
+// Função para deixar de exibir a janela do chatbot
+function exit_chatbot() { 
   chatbot_window.style.cssText = 'display: none';
   chatbot_button.style.cssText = 'display: flex';
   chatbot_input.value='';
   document.getElementById("chatbot_text_id").querySelectorAll("p").forEach(p => {p.parentNode.removeChild(p)});
 };
 
+// Função para detectar o clique do "enter" + utilizar o conteudo escrito
 chatbot_input.addEventListener('keyup', function(e){
   let key = e.which || e.keyCode;
   if ((key == 13) && (this.value != '')){
@@ -57,19 +60,27 @@ chatbot_input.addEventListener('keyup', function(e){
 //
 //
 
+// Função para associar o que foi escrito ao script
 function search_memory(jsonObject, input){
   let script = jsonObject.script;
+  if(!isNaN(parseInt(input))){
+    input = parseInt(input)  
+  }
+
   switch (input){
     case "inicio":
       chatbot_response_text(script.script_inicial);
       break
-    case "ajuda":
+    case "iniciar":
       chatbot_response_text(script.ajuda.script_ajuda);
+      break
+    case "menu":
+      document.getElementById("chatbot_text_id").querySelectorAll("p").forEach(p => {p.parentNode.removeChild(p)});
+      searchJsonFile("iniciar")
       break
     case "faq":
       for(i=0; i<99999; i++){
-        chatbot_response_text(script.ajuda.faq[i][0]);
-        chatbot_response_text(script.ajuda.faq[i][1])
+        chatbot_response_text(script.ajuda.faq[i][1]);
       }
       break
     case "site":
@@ -79,23 +90,31 @@ function search_memory(jsonObject, input){
       for(i=0; i<3; i++){
         chatbot_response_text(script.ajuda.contatos[i]);
       }
-      
       break
     case "recomendar":
       let x = Math.floor(Math.random() * 10)
       chatbot_response_text(script.recomendar[x][1]);
       break
     case "curiosidades":
-      for(i=0; i<8; i++){
-        chatbot_response_text(script.curiosidades[i]);
-      } 
+      let y = Math.floor(Math.random() * 9)
+        chatbot_response_text(script.ajuda.curiosidades[y]);
       break
     default:
-      chatbot_response_text(script.script_default);
-      break
+      if (typeof(input) === "number"){
+        for(i=0; i<99999; i++){
+          if(i == input-1){
+            chatbot_response_text(script.ajuda.faq[i][2]);
+          }
+        }
+      }
+      else{
+        chatbot_response_text(script.script_default);
+        break
+      }
   }
 }
 
+// Função para gerar um texto do usuario
 function user_text(input) {
   const node = document.createElement("p");
   node.style.textAlign = 'right';
@@ -105,6 +124,7 @@ function user_text(input) {
   document.getElementById("chatbot_text_id").appendChild(node);
 }
 
+// Função para gerar um texto do bot
 function chatbot_response_text(text){
   let node = document.createElement("p");
   let textnode = document.createTextNode(text);
@@ -113,6 +133,7 @@ function chatbot_response_text(text){
   document.getElementById('chatbot_text_id').scrollTop -= 999999999999;
 }
 
+// Função para carregar o JSON
 function searchJsonFile(keyword) {
   fetch(chatbot_memory)
     .then(response => response.json())
