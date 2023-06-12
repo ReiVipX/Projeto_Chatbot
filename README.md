743c1167a93d70057c414d5e20f0f28376c64a20
ReiVipX <lucasgrcayres@gmail.com>
2023-06-11 23:52:34 -0300

    Modificação de carrinho, adicionada animações, adicionado footer, mudança de header, mudança no html e melhorias gerais de estilo

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Projeto-Uninove
-Kalango Games - site
+## Proposta de site e-commerce chamado Kalango Games
+### Queriamos fazer um site com uma proposta de ser mais do que apenas um e-commerce, onde a comunidade pudesse tambem ser encaminhada para interagir em nosso discord (fictício), fazer amizades e realizar torneios para fortaceler a compettitividade saudável!
---
+++ json/Ofertas.js
@@ -0,0 +1,69 @@
+let ofertaJson=[
+    
+    {id:18, 
+        name:"Headset Gamer Havit, Drivers 53mm, Microfone Plugável, P2, PC, PS4, XBOX ONE, Preto - HV-H2002D",
+        img:'/imgs/imagens_produtos/HeadSetGamer.jpeg',
+        price:248.65,
+        desc: 0.3},
+
+    {id:19, 
+        name:"Jogo Hogwarts Legacy Deluxe ED, PS5", 
+        img:'/imgs/imagens_produtos/Hogwarts Legacy.jpeg',
+        price:329.99,
+        desc: 0.1},
+
+    {id:20, 
+        name:"Memória Kingston Fury Beast, 8GB, 3200MHz, DDR4, CL16, Preto - KF432C16BB/8", 
+        img:'/imgs/imagens_produtos/MemoriaKING.jpeg',
+        price:211.75,
+        desc: 0.2},
+
+    {id:21, 
+        name:"Jogo Resident Evil 4, PS5", 
+        img:'/imgs/imagens_produtos/Re4.jpeg',
+        price:299.99,
+        desc: 0.2},
+
+    {id:22, 
+        name:"Placa de Vídeo RTX 3060 Ti Dual OC Asus NVIDIA GeForce, 8 GB GDDR6X, DLSS, Ray Tracing - DUAL-RTX3060TI-O8GD6X", 
+        img:'/imgs/imagens_produtos/RTX360.jpeg',
+        price:3411.75,
+        desc: 0.1},
+
+    {id:23, 
+        name:"Jogo Sackboy: Uma Grande Aventura PS5", 
+        img:'/imgs/imagens_produtos/SackBoy.jpeg',
+        price:329.99,
+        desc: 0.1},
+
+    {id:24, 
+        name:"SSD 1 TB Kingston NV2, M.2 2280 PCIe, NVMe, Leitura: 3500 MB/s e Gravação: 2100 MB/s - SNV2S/1000G", 
+        img:'/imgs/imagens_produtos/SSD1tb.jpeg',
+        price:306.99,
+        desc: 0.4},
+
+    {id:25, 
+        name:"SSD 480 GB Kingston A400, SATA, Leitura: 500MB/s e Gravação: 450MB/s - SA400S37/480G", 
+        img:'/imgs/imagens_produtos/SSDKing.jpeg',
+        price:306.99,
+        desc: 0.6},
+
+    {id:26, 
+        name:"Jogo Star Wars Jedi: Survivor, PS5", 
+        img:'/imgs/imagens_produtos/StarWars.jpeg',
+        price:299.99,
+        desc: 0.5},
+
+    {id:27, 
+        name:"Jogo Street Fighter 6, PS5", 
+        img:'/imgs/imagens_produtos/Street Figh.jpeg',
+        price:299.99,
+        desc: 0.5},
+
+    {id:28, 
+        name:"Jogo Uncharted: Coleção Legado dos Ladrões, PS5", 
+        img:'/imgs/imagens_produtos/Uncharted.jpeg',
+        price:349.99,
+        desc: 0.15},
+
+    ]
